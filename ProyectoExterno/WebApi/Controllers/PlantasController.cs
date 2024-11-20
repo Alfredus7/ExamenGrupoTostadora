c8877e18ffa24a543c368616779bb4ef45b7498e
rodr1gounivalle20 <fpr4003761@est.univalle.edu>
2024-11-20 19:55:01 -0400

    misPartesfreddy

---
+++ ProyectoExterno/WebApi/Controllers/PlantasController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlantasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlantasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Plantas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Planta>>> GetPlantas()
+        {
+            return await _context.Plantas.ToListAsync();
+        }
+
+        // GET: api/Plantas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Planta>> GetPlanta(int id)
+        {
+            var planta = await _context.Plantas.FindAsync(id);
+
+            if (planta == null)
+            {
+                return NotFound();
+            }
+
+            return planta;
+        }
+
+        // PUT: api/Plantas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlanta(int id, Planta planta)
+        {
+            if (id != planta.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(planta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlantaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Plantas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Planta>> PostPlanta(Planta planta)
+        {
+            _context.Plantas.Add(planta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlanta", new { id = planta.Id }, planta);
+        }
+
+        // DELETE: api/Plantas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlanta(int id)
+        {
+            var planta = await _context.Plantas.FindAsync(id);
+            if (planta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Plantas.Remove(planta);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PlantaExists(int id)
+        {
+            return _context.Plantas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ProyectoExterno/WebApi/Controllers/TipoPlantasController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoPlantasController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TipoPlantasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TipoPlantas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TipoPlanta>>> GetTipoPlantas()
+        {
+            return await _context.TipoPlantas.ToListAsync();
+        }
+
+        // GET: api/TipoPlantas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TipoPlanta>> GetTipoPlanta(int id)
+        {
+            var tipoPlanta = await _context.TipoPlantas.FindAsync(id);
+
+            if (tipoPlanta == null)
+            {
+                return NotFound();
+            }
+
+            return tipoPlanta;
+        }
+
+        // PUT: api/TipoPlantas/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTipoPlanta(int id, TipoPlanta tipoPlanta)
+        {
+            if (id != tipoPlanta.TipoPlantaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(tipoPlanta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TipoPlantaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TipoPlantas
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TipoPlanta>> PostTipoPlanta(TipoPlanta tipoPlanta)
+        {
+            _context.TipoPlantas.Add(tipoPlanta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTipoPlanta", new { id = tipoPlanta.TipoPlantaId }, tipoPlanta);
+        }
+
+        // DELETE: api/TipoPlantas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTipoPlanta(int id)
+        {
+            var tipoPlanta = await _context.TipoPlantas.FindAsync(id);
+            if (tipoPlanta == null)
+            {
+                return NotFound();
+            }
+
+            _context.TipoPlantas.Remove(tipoPlanta);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TipoPlantaExists(int id)
+        {
+            return _context.TipoPlantas.Any(e => e.TipoPlantaId == id);
+        }
+    }
+}
